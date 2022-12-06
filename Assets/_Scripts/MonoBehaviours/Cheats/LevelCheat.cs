59b8a797eaa022acd30ac840e483f436e112a133
Vladislav2123 <mangushev211104@gmail.com>
2022-12-06 11:57:12 +0300

    LevelsManager Cheat Added

---
+++ Assets/_Scripts/MonoBehaviours/Cheats/LevelCheat.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using Zenject;
+using TMPro;
+using System.Collections.Generic;
+
+public class LevelCheat : MonoBehaviour
+{
+    [SerializeField] private TMP_Dropdown _dropDown;
+
+    [Inject] private LevelsManager _levelsManager;
+
+
+    private void Awake()
+    {
+        InitOptions();
+    }
+
+    private void InitOptions()
+    {
+        List<string> options = new List<string>(_levelsManager.Levels.Length);
+
+        for(int i = 1; i <= _levelsManager.Levels.Length; i ++)
+        {
+            options.Add($"Level {i}");
+        }
+
+        _dropDown.AddOptions(options);
+    }
+
+    public void OnValueChanged(int value)
+    {
+        _levelsManager.CompletedLevels = value;
+        _levelsManager.CurrentLevelNumber = value;
+        _levelsManager.RestartLevel();
+    }
+}

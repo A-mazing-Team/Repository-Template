7d2a291f026273abee26b5aec2e474a8f867aaf5
sergezhu <zs.inkvizitor@gmail.com>
2023-03-28 00:20:26 +0300

    json utils serializer wrappers

---
+++ Assets/_Scripts/SaveSystem2/SaveData/Serialization/JsonUtilityPrettyPrintSerializer.cs
@@ -0,0 +1,29 @@
+﻿namespace _Scripts.SaveSystem2.SaveData.Serialization
+{
+	using System;
+	using UnityEngine;
+
+	public class JsonUtilityPrettyPrintSerializer : ISerializer
+	{
+		public byte[] SerializeBinary( object obj )
+		{
+			throw new InvalidOperationException( "Binary serialize not supported" );
+		}
+
+		public object DeserializeBinary( byte[] bytes )
+		{
+			throw new InvalidOperationException( "Binary serialize not supported" );
+		}
+
+		public string SerializeJSON<T>( T obj )
+		{
+			return JsonUtility.ToJson( obj, true );
+		}
+
+		public T DeserializeJSON<T>( string json )
+		{
+			return JsonUtility.FromJson<T>( json );
+		}
+	}
+}
+
